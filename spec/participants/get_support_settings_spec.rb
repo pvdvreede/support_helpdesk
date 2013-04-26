c31f742b5447616855b16db34658d7a29510e443
pvdvreede <paul@vdvreede.net>
2013-04-26 15:19:05 +1000

    Add more tests for support settings participant.

--- spec/participants/get_support_settings_spec.rb
+++ spec/participants/get_support_settings_spec.rb
@@ -25,13 +25,146 @@ describe Support::Participants::GetGlobalSettings do
       $reply.fields['cancel'].should be_true
     end
 
-    it 'has support setting with cc only'
-    it 'has support setting with to only'
-    it 'has support setting with both'
+    it 'has support setting with cc only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => false,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+    it 'has support setting with to only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => false
+      )
+      participant.on_workitem
+      $reply.fields['support_settings'].should_not be_nil
+      $reply.fields['support_settings']['name'].should eq support.name
+    end
+
+
+    it 'has support setting with both' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['support_settings'].should_not be_nil
+      $reply.fields['support_settings']['name'].should eq support.name
+    end
   end
 
-  context 'has support email as cc'
+  context 'has support email as cc' do
+    let(:email)     { Mail::Message.new(:cc => 'support@test.com') }
 
-  context 'does not have support email'
+    it 'has support setting disabled' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :active => false
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+    it 'has support setting with to only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => false
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+    it 'has support setting with cc only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => false,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['support_settings'].should_not be_nil
+      $reply.fields['support_settings']['name'].should eq support.name
+    end
+
+
+    it 'has support setting with both' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['support_settings'].should_not be_nil
+      $reply.fields['support_settings']['name'].should eq support.name
+    end
+  end
+
+  context 'does not have support email' do
+    let(:email)     { Mail::Message.new(:cc => 'nosupport@test.com') }
+
+    it 'has support setting disabled' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :active => false
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+    it 'has support setting with to only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => false
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+    it 'has support setting with cc only' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => false,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+
+
+    it 'has support setting with both' do
+      support = FactoryGirl.create(
+        :support_helpdesk_setting,
+        :to_email_address => "support@test.com",
+        :search_in_to => true,
+        :search_in_cc => true
+      )
+      participant.on_workitem
+      $reply.fields['cancel'].should_not be_nil
+      $reply.fields['cancel'].should be_true
+    end
+  end
 
 end
